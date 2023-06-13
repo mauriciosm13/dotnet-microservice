5ddfdbb65b358487e8efb597e1839300243c4e5d
Maurício Mendonça <70268675+mauriciosm13@users.noreply.github.com>
2023-06-13 08:23:30 -0300

    Commit Post in restaurante service

---
+++ dotnet-microservice/RestauranteService/ItemServiceHttpClient/ItemServiceHttpClient.cs
@@ -0,0 +1,28 @@
+﻿using RestauranteService.Dtos;
+using System.Text;
+using System.Text.Json;
+
+namespace RestauranteService.ItemServiceHttpClient
+{
+    public class ItemServiceHttpClient : IItemServiceHttpClient
+    {
+        private readonly HttpClient _client;
+        private readonly IConfiguration _configuration;
+
+        public ItemServiceHttpClient(HttpClient client)
+        {
+            _client = client;
+        }
+        public async void EnviaRestauranteParaItemService(RestauranteReadDto readDto)
+        {
+            var contentHttp = new StringContent
+                (
+                    JsonSerializer.Serialize(readDto),
+                    Encoding.UTF8,
+                    "application/json"
+                );
+
+            await _client.PostAsync(_configuration["ItemService"], contentHttp);
+        }
+    }
+}
