a2a23d6ee88f52c6282363eedba083f69bcb2e26
Maurício Mendonça <70268675+mauriciosm13@users.noreply.github.com>
2023-06-13 09:10:37 -0300

    Commit

--- dotnet-microservice/RestauranteService/Controllers/RestauranteController.cs
+++ dotnet-microservice/RestauranteService/Controllers/RestauranteController.cs
@@ -56,9 +56,6 @@ public class RestauranteController : ControllerBase
 
         _itemServiceHttpClient.EnviaRestauranteParaItemService(restauranteReadDto);
 
-
-        _itemServiceHttpClient.EnviaRestauranteParaItemService(restauranteReadDto);
-
         return CreatedAtRoute(nameof(GetRestauranteById), new { restauranteReadDto.Id }, restauranteReadDto);
     }
 }
